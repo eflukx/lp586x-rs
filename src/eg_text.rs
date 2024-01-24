64561cfd61d30f97e7eb619add768dcd67fa89f6
Rogier Lodewijks <rogier@lodewijks.org>
2024-01-24 15:35:40 +0100

    Text scroll object does not hold on to display, so multiple can easily instantiated and shared (and e.g. stored in a Vec)

--- src/eg_text.rs
+++ src/eg_text.rs
@@ -1,4 +1,6 @@
 extern crate alloc;
+use core::marker::PhantomData;
+
 use alloc::boxed::Box;
 
 use embedded_graphics::{
@@ -10,9 +12,9 @@ use embedded_hal::digital::v2::OutputPin;
 
 use crate::{egfx::Lp586xDisplay1x2, PwmAccess};
 
-pub trait DrawHText {
+pub trait HScroll {
     type Error;
-    type Ouput;
+    type Output;
 
     const DEFAULT_FONT: MonoFont<'static> = mono_font::ascii::FONT_5X8;
     const DEFAULT_TEXT_FRONT_STYLE: MonoTextStyle<'static, Gray8> =
@@ -24,53 +26,54 @@ pub trait DrawHText {
         &mut self,
         // text: &str,
         text_drawable: &mut Text<'_, MonoTextStyle<'_, Gray8>>,
-        x_offset: i32,
-    ) -> Result<Option<Self::Ouput>, Self::Error>;
+        position: Point,
+    ) -> Result<Option<Self::Output>, Self::Error>;
 
     fn initial_x_offset(&self) -> i32;
 
-    fn default_y_offset(&self) -> i32 {
-        // maybe fix this to 6?
-        Self::DEFAULT_TEXT_FRONT_STYLE.line_height() as i32 - 2
+    // At which position do we start scrollin'?
+    fn default_offset(&self) -> Point {
+        Point::new(0, Self::DEFAULT_TEXT_FRONT_STYLE.line_height() as i32 - 2)
     }
 
-    fn scroll_x_offsets_iter(
+    fn h_scroll_position_iter(
         &self,
         drawable: impl Dimensions,
-    ) -> impl Iterator<Item = i32> + 'static;
+    ) -> impl Iterator<Item = Point> + 'static;
 }
 
-impl<D, VP> DrawHText for Lp586xDisplay1x2<D, VP>
+impl<D, VP> HScroll for Lp586xDisplay1x2<D, VP>
 where
     D: PwmAccess<u8> + OriginDimensions,
     VP: OutputPin,
 {
     type Error = D::Error;
-    type Ouput = Point;
+    type Output = Point;
 
     fn draw_scrolltext_frame(
         &mut self,
-        // text: &str,
         text_drawable: &mut Text<'_, MonoTextStyle<'_, Gray8>>,
-        x_offset: i32,
+        position: Point,
     ) -> Result<Option<Point>, D::Error> {
-        // let mut t = Text::new(text, position, Self::DEFAULT_TEXT_FRONT_STYLE);
         text_drawable.character_style = Self::DEFAULT_TEXT_FRONT_STYLE;
-        text_drawable.position = Point::new(x_offset, self.default_y_offset());
+        text_drawable.position = position + self.default_offset();
         text_drawable.draw(self)?;
 
-        self.toggle_sync(); // Draw!
+        self.toggle_sync(); // Show!
 
         text_drawable.character_style = Self::DEFAULT_TEXT_WIPE_STYLE;
         text_drawable.draw(self).map(Option::Some)
     }
 
-    fn scroll_x_offsets_iter(
+    fn h_scroll_position_iter(
         &self,
         drawable: impl Dimensions,
-    ) -> impl Iterator<Item = i32> + 'static {
+    ) -> impl Iterator<Item = Point> + 'static {
         let drawable_width = drawable.bounding_box().size.width as i32;
-        (-drawable_width..=self.initial_x_offset()).rev().clone()
+        (-drawable_width..=self.initial_x_offset())
+            .rev()
+            .map(|x_off| Point::new(x_off, 0))
+            .clone()
     }
 
     fn initial_x_offset(&self) -> i32 {
@@ -78,41 +81,45 @@ where
     }
 }
 
-pub struct TextScroller<'a, D> {
-    display: &'a mut D,
-    x_iter: Box<dyn Iterator<Item = i32>>,
+pub struct ScrollIter<'a, D> {
+    display: PhantomData<D>,
+    position_iter: Box<dyn Iterator<Item = Point>>,
     text_drawable: Text<'a, MonoTextStyle<'a, Gray8>>,
 }
 
-impl<'a, D> TextScroller<'a, D>
+impl<'a, D> ScrollIter<'a, D>
 where
-    D: DrawHText,
+    D: HScroll,
 {
-    pub fn new(display: &'a mut D, text: &'a str) -> Self {
+    pub fn new(display: &D, text: &'a str) -> Self {
         let text_drawable = Text::new(text, Point::zero(), D::DEFAULT_TEXT_FRONT_STYLE);
-        let x_iter = display.scroll_x_offsets_iter(text_drawable);
+        let x_iter = display.h_scroll_position_iter(text_drawable);
 
         Self {
-            display,
-            x_iter: Box::new(x_iter),
+            display: PhantomData,
+            position_iter: Box::new(x_iter),
             text_drawable,
+        }
+    }
+
+    pub fn draw_next_frame(&mut self, display: &mut D) -> Result<Option<Point>, D::Error> {
+        match self.next() {
+            Some(offset) => {
+                display.draw_scrolltext_frame(&mut self.text_drawable, offset)?;
+                Ok(Some(offset))
+            }
+            None => Ok(None),
         }
     }
 }
 
-impl<'a, D> Iterator for TextScroller<'a, D>
+impl<'a, D> Iterator for ScrollIter<'a, D>
 where
-    D: DrawHText,
+    D: HScroll,
 {
-    type Item = i32;
+    type Item = Point;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let x_offset = self.x_iter.next()?;
-        self.display
-            .draw_scrolltext_frame(&mut self.text_drawable, x_offset)
-            .ok()
-            .unwrap();
-
-        Some(x_offset)
+        self.position_iter.next()
     }
 }
